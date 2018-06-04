40fd8d0d4af1e50a4f1ef99414c89ee76ea9d162
manson112 <rlarhskdn777@naver.com>
2018-06-04 21:50:57 +0900

    first stage

--- 1545.cpp
+++ 1545.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int test_num;
 
+    cin >> test_num;
+    
+    for(int i=test_num; i>=0; i--) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
